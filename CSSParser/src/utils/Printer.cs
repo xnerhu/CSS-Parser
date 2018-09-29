a133d2f7e6a73cafd53fb45d3913d5203d7a5782
unknown <xnerhu@gmail.com>
2018-09-29 12:10:13 +0200

    Some fixes

--- CSSParser/src/utils/Printer.cs
+++ CSSParser/src/utils/Printer.cs
@@ -6,14 +6,15 @@ namespace CSSParser {
             foreach (StyleRule rule in rules) {
                 PrintColored(rule.SelectorText, DefaultColors.Selector, true);
                 PrintColored(" {\n", DefaultColors.Bracket, true);
+
                 foreach (StyleDeclaration declaration in rule.Style) {
-                    Console.Write(new string(' ', 4));
-                    PrintColored(declaration.Property, DefaultColors.Property, true);
+                    PrintColored(new string(' ', 4) + declaration.Property, DefaultColors.Property, true);
                     PrintColored(": ", DefaultColors.Semicolon, true);
                     PrintColored(declaration.Value, DefaultColors.Value, true);
                     PrintColored(";\n", DefaultColors.Semicolon, true);
                 }
-                PrintColored("}\n", DefaultColors.Bracket, true);
+
+                PrintColored("}\n\n", DefaultColors.Bracket, true);
             }
         }
         private static void PrintColored(string text, ConsoleColor color, bool inline = false) {
